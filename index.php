532ab82b63df3f93ee3b8aed7de9a8eaadaa6191
AnnaOzer <ozerskaya.ru@mail.ru>
2016-01-31 22:36:29 +0300

    + тестирование метода query() класса Db

--- index.php
+++ index.php
@@ -9,6 +9,6 @@ require __DIR__ . '/autoload.php';
 
 $db = new \App\Db();
 
-$res = $db->execute('CREATE TABLE `foo` (`id` SERIAL)');// ожидаем true при первом запуске, false при последующих
-var_dump($res);
+$data = $db->query('SELECT * FROM `foo`');
+var_dump($data);
 
