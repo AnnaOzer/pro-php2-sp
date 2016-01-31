b5c9b692152df49ce517a4e1644a7e97953040bd
AnnaOzer <ozerskaya.ru@mail.ru>
2016-01-31 19:58:03 +0300

    + class Db

---
+++ App/Db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Пользователь
+ * Date: 31.01.2016
+ * Time: 20:54
+ */
+
+namespace App;
+
+
+class Db {
+    public function __construct()
+    {
+        echo 'Hello DB!';
+    }
+} 
