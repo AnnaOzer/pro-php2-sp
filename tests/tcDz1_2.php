e58439513ceddb3fd7b1c15932c9254ded3f0fc1
AnnaOzer <ozerskaya.ru@mail.ru>
2016-02-03 20:55:00 +0300

    + к дз 1-2: продолжаю тестировать метод execute()

---
+++ tests/tcDz1_2.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Пользователь
+ * Date: 03.02.2016
+ * Time: 19:51
+ */
+
+namespace tests;
+
+
+use App\Model;
+
+class tcDz1_2
+extends Model
+{
+    const TABLE='dz1_2';
+
+    public $art;
+    public $name;
+
+} 
--- tests/testDz1_2.php
+++ tests/testDz1_2.php
@@ -15,6 +15,7 @@
 require __DIR__ . '/../autoload.php';
 
 use App\Db;
+use tests\tcDz1_2;
 
 $db = new Db();
 
@@ -29,9 +30,15 @@ var_dump($res1); //bool(true) первый раз; создана таблица
 
 // работоспособность метода execute() с подстановками
 
-$param =[':art'=>'1358064', ':name'=>'Стол'];
+$param2 =[':art'=>'1358064', ':name'=>'Стол'];
+$res2 = $db->execute('INSERT INTO dz1_2 (art, name) VALUES (:art, :name);', $param2);
+var_dump($res2); // bool(true), в таблицу dz1_2 вставлена строка  - артикул 1358064, название Стол
 
-$res2 = $db->execute('INSERT INTO dz1_2 (art, name) VALUES (:art, :name);', $param);
+$param3 =[':art'=>'69791459', ':name'=>'Стул'];
+$res3 = $db->execute('INSERT INTO dz1_2 (art, name) VALUES (:art, :name);', $param3);
+var_dump($res3); // bool(true), в таблицу dz1_2 вставлена строка  - артикул 69791459, название Стул
 
-var_dump($res2); // bool(true), в таюлицу dz1_2 вставлена строка  - артикул 1358064, название Стол
+$param4 =[':art'=>'919851', ':name'=>'Шкаф'];
+$res4 = $db->execute('INSERT INTO dz1_2 (art, name) VALUES (:art, :name);', $param4);
+var_dump($res4); // bool(true), в таблицу dz1_2 вставлена строка  - артикул 919851, название Шкаф
 
